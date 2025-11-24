51b4fc40d98d1c23d57e6e5c8d59b57d4060fcf2
richardbolland <richardbolland01@gmail.com>
2025-11-24 22:18:53 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // THIS MUST MATCH YOUR REPO NAME SLASHES
 })
